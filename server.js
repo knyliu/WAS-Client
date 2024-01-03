37a545df562c68eff88f166de462fb7d96255c61
knyliu <41171123h@ntnu.edu.tw>
2024-01-03 11:20:32 +0800

    All New with wec.config

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express"; //create API, served Front-End
+import cors from "cors"; // set up the commnuication rules between Front-End and Back-End
+import mongoose from "mongoose";
+
+import { userRouter } from "./routes/user.js";
+import { appointmentsRouter } from "./routes/route_appointment.js";
+
+
+const app = express(); // a version of our API
+
+app.use(express.json()); // the data got from the Front-End will be converted into JSON
+app.use(cors());
+
+app.use("/auth", userRouter); // applt the router
+app.use("/appointments", appointmentsRouter);
+app.use("/client_appointments", appointmentsRouter);
+
+mongoose.connect(
+  "mongodb+srv://41171123h:41171123hB1@b1.3xjj9xw.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const port = process.env.PORT || 3001;
+// production script
+app.use(express.static("./client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
